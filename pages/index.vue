423546f15feff3757d116fcb9c09f12e0dc2fa96
hysdsk <26004436+hysdsk@users.noreply.github.com>
2023-05-03 00:51:29 +0900

    chore: ElementPlusを導入する

--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,57 @@
 <template>
-  <h1 class="">出来高増加銘柄一覧</h1>
-  <div class="">
-    <table class="">
-      <thead>
-      <tr>
-        <th>銘柄コード</th>
-        <th>銘柄名</th>
-        <th>業種名</th>
-        <th>直近出来高</th>
-        <th>過去出来高</th>
-        <th>増加率</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="symbol in data.increaseVolumeSymbols" :key="symbol.code">
-        <td>
-          <NuxtLink :to="`/symbol/${symbol.symbolCode}`" target="_blank">
-            {{ symbol.symbolCode }}
-          </NuxtLink>
-        </td>
-        <td>{{ symbol.symbolName }}</td>
-        <td>{{ symbol.bisCategoryName }}</td>
-        <td style="text-align: end;">{{ symbol.recentVolume.toLocaleString() }}</td>
-        <td style="text-align: end;">{{ symbol.averageVolume.toLocaleString() }}</td>
-        <td style="text-align: end;">+ {{ symbol.increaseRate }}%</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+  <el-text size="large" tag="b">出来高増加銘柄一覧</el-text>
+  <el-container>
+    <el-main>
+      <el-table :data="data.increaseVolumeSymbols" @row-click="moveTo">
+        <el-table-column prop="symbolCode" label="銘柄コード"/>
+        <el-table-column prop="symbolName" label="銘柄名"/>
+        <el-table-column prop="bisCategoryName" label="業種名"/>
+        <el-table-column prop="recentVolume" label="直近出来高" align="right" :formatter="formatNum"/>
+        <el-table-column prop="averageVolume" label="過去出来高" align="right" :formatter="formatNum"/>
+        <el-table-column prop="increaseRate" label="増加率" align="right" :formatter="formatRate"/>
+      </el-table>
+    </el-main>
+  </el-container>
 
-  <h1 class="">上昇銘柄一覧</h1>
-  <div class="">
-    <table class="">
-      <thead>
-      <tr>
-        <th>銘柄コード</th>
-        <th>銘柄名</th>
-        <th>業種名</th>
-        <th>平均出来高</th>
-        <th>終値</th>
-        <th>増加率</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="symbol in data.increasePriceSymbols" :key="symbol.code">
-        <td>
-          <NuxtLink :to="`/symbol/${symbol.symbolCode}`" target="_blank">
-            {{ symbol.symbolCode }}
-          </NuxtLink>
-        </td>
-        <td>{{ symbol.symbolName }}</td>
-        <td>{{ symbol.bisCategoryName }}</td>
-        <td style="text-align: end;">{{ symbol.averageVolume.toLocaleString() }}</td>
-        <td style="text-align: end;">{{ symbol.closingPrice.toLocaleString() }}</td>
-        <td style="text-align: end;">+ {{ symbol.increaseRate }}%</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+  <el-text size="large" tag="b">上昇銘柄一覧</el-text>
+    <el-container>
+      <el-main>
+        <el-table :data="data.increasePriceSymbols" @row-click="moveTo">
+          <el-table-column prop="symbolCode" label="銘柄コード"/>
+          <el-table-column prop="symbolName" label="銘柄名"/>
+          <el-table-column prop="bisCategoryName" label="業種名"/>
+          <el-table-column prop="averageVolume" label="平均出来高" align="right" :formatter="formatNum"/>
+          <el-table-column prop="closingPrice" label="終値" align="right" :formatter="formatNum"/>
+          <el-table-column prop="increaseRate" label="増加率" align="right" :formatter="formatRate"/>
+        </el-table>
+      </el-main>
+    </el-container>
 
-  <h1 class="">踏み上げ期待銘柄一覧</h1>
-  <div class="">
-    <table class="">
-      <thead>
-      <tr>
-        <th>銘柄コード</th>
-        <th>銘柄名</th>
-        <th>業種名</th>
-        <th>平均出来高</th>
-        <th>信用売り残</th>
-        <th>信用買い残</th>
-        <th>信用倍率</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="symbol in data.increaseSellBalance" :key="symbol.code">
-        <td>
-          <NuxtLink :to="`/symbol/${symbol.symbolCode}`" target="_blank">
-            {{ symbol.symbolCode }}
-          </NuxtLink>
-        </td>
-        <td>{{ symbol.symbolName }}</td>
-        <td>{{ symbol.bisCategoryName }}</td>
-        <td style="text-align: end;">{{ symbol.averageVolume.toLocaleString() }}</td>
-        <td style="text-align: end;">{{ symbol.sellBalance.toLocaleString() }}</td>
-        <td style="text-align: end;">{{ symbol.buyBalance.toLocaleString() }}</td>
-        <td style="text-align: end;">+ {{ symbol.balanceRate }}%</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+  <el-text size="large" tag="b">踏み上げ期待銘柄一覧</el-text>
+  <el-container>
+    <el-main>
+      <el-table :data="data.increaseSellBalance" @row-click="moveTo">
+        <el-table-column prop="symbolCode" label="銘柄コード"/>
+        <el-table-column prop="symbolName" label="銘柄名"/>
+        <el-table-column prop="bisCategoryName" label="業種名"/>
+        <el-table-column prop="averageVolume" label="平均出来高" align="right" :formatter="formatNum"/>
+        <el-table-column prop="sellBalance" label="信用売り残" align="right" :formatter="formatNum"/>
+        <el-table-column prop="buyBalance" label="信用買い残" align="right" :formatter="formatNum"/>
+        <el-table-column prop="balanceRate" label="信用倍率" align="right" :formatter="formatRate"/>
+      </el-table>
+    </el-main>
+  </el-container>
 </template>
 
 <script setup>
   const { data } = await useFetch("/api/symbols");
+  const moveTo = (e) => {
+    window.open(`/symbol/${e.symbolCode}`, '_blank')
+  }
+  const formatNum = (r, c, v, i) => {
+    return v.toLocaleString()
+  }
+  const formatRate = (r, c, v, i) => {
+    return `+${v}%`
+  }
 </script>
