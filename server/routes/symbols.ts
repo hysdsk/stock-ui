74a9f13e1d93775097e7e6975254f653fedda0d7
hysdsk <26004436+hysdsk@users.noreply.github.com>
2023-06-10 16:23:26 +0900

    feat: 銘柄情報取得APIを設ける

---
+++ server/routes/symbols.ts
@@ -0,0 +1,51 @@
+import * as mysql from "mysql2";
+const config = useRuntimeConfig()
+
+const connection = mysql.createConnection({
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPswd,
+    database: config.dbName
+});
+
+interface Symbol {
+    symbolCode: string;
+    symbolName: string;
+    totalStocks: number;
+}
+
+export default defineEventHandler(async (event: any) => {
+    const query = getQuery(event);
+    if (!query.hasOwnProperty("code") || query.code == null) {
+        return [];
+    }
+    const codelist = String(query.code).split(",");
+    let p: Promise<any> = new Promise((resolve, reject) => {
+        const symbol_detail_sql: string = `
+        SELECT
+            code,
+            name,
+            total_stocks
+        FROM
+            kabu.symbols
+        WHERE
+            code in (${new Array<String>(codelist.length).fill("?").join(",")})
+        `;
+        connection.query(symbol_detail_sql, codelist, (err, rows, fields) => {
+            resolve(rows);
+        });
+    });
+    const symbols: Symbol[] = await p.then((reslut) => {
+        return reslut.map((e: any) => <Symbol>{
+            symbolCode: String(e.code),
+            symbolName: String(e.name),
+            totalStocks: Number(e.total_stocks)
+        });
+    });
+
+    if (symbols.length == 1) {
+        return symbols[0];
+    } else {
+        return symbols;
+    }
+})
