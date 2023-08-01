7baa3bcb5faafb505d96fe95ac4e5b37a04ab863
hysdsk <26004436+hysdsk@users.noreply.github.com>
2023-08-01 22:19:53 +0900

    chore: 不要なコードを削除する

--- pages/notices.vue
+++ pages/notices.vue
@@ -2,7 +2,7 @@
   <el-container>
     <el-main>
       <el-card>
-        <el-table ref="multipleTableRef" :data="Object.values(ranklist)" row-key="code" :row-class-name="colorRows" @row-click="(r, c, e) => { copyToClipboard(r.code) }" style="width: 100%">
+        <el-table ref="multipleTableRef" :data="Object.values(ranklist)" row-key="code" :row-class-name="colorRows" @row-click="(r, c, e) => { copyToClipboard(r.code) }" style="width: 100%" height="896">
           <el-table-column type="selection" header-align="center"  align="center" width="50" reserve-selection/>
           <el-table-column prop="code" label="コード" header-align="center" align="center" width="100" sortable />
           <el-table-column prop="name" label="銘柄名" header-align="center" :formatter="formatName" sortable/>
@@ -112,12 +112,10 @@
 <script lang="ts" setup>
   import { io } from "socket.io-client";
   import { reactive, ref, onMounted, h } from "vue";
-  import { Filter } from "@element-plus/icons-vue";
   import { ElNotification, ElTable } from 'element-plus';
 
   const config = useRuntimeConfig().public;
   const multipleTableRef = ref<InstanceType<typeof ElTable>>();
-  const colnum = ref(6);
   const filtered = ref(false);
   const selectedSymbols = ref([]);
   const symbols = reactive({});
