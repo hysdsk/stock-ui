f20880a29e56082c05e8a5502ac44818f5dd3540
hysdsk <26004436+hysdsk@users.noreply.github.com>
2023-10-14 12:55:06 +0900

    feat: 市場全体の売買代金を表示する

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,54 @@
 <template>
   <el-container>
     <el-main>
-      考え中...
+      <el-text size="large" tag="b" type="primary">市場別売買代金推移</el-text>
+      <canvas id="chart" style="width: 100%; height: 1024px"></canvas>
     </el-main>
   </el-container>
 </template>
 
 <script lang="ts" setup>
-  
+  import Chart from "chart.js/auto";
+  import { onMounted } from "vue";
+
+  const { data } = await useFetch("/api/market/tradingValues");
+  const barData = {
+    labels: data._value.prime.openingDate,
+    datasets: [
+      {label: "Prime", data: data._value.prime.tradingValue, backgroundColor: "#ff4569"},
+      {label: "Standard", data: data._value.standard.tradingValue, backgroundColor: "#33eb91"},
+      {label: "Growth", data: data._value.growth.tradingValue, backgroundColor: "#33eaff"},
+    ]
+  }
+
+  const options = {
+    responsive: false,
+    title: {
+      text: "",
+    },
+    scales: {
+      y: {
+        title: { display: true, text: "売買代金（千円）" },
+        stacked: true
+      },
+      x: {
+        title: { display: true, text: "開場日"},
+        stacked: true
+      },
+    },
+    plugins: {
+      legend: {
+        position: "top"
+      }
+    }
+  }
+
+  onMounted(() => {
+    console.log(barData)
+    new Chart(document.getElementById("chart"), {
+      type: "bar",
+      data: barData,
+      options: options,
+    })
+  });
 </script>
---
+++ server/api/market/tradingValues.ts
@@ -0,0 +1,103 @@
+import * as mysql from "mysql2";
+import dayjs from 'dayjs';
+dayjs.locale("ja");
+const config = useRuntimeConfig()
+
+const pool = mysql.createPool({
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPswd,
+    database: config.dbName
+});
+
+interface TradingValueByDivision {
+    openingDate: string;
+    divisionCode: string;
+    divisionName: string;
+    tradingValue: number;
+}
+
+const getOpeningDates = (tvbd: TradingValueByDivision) => {
+    const year = tvbd.openingDate.substring(0, 4);
+    const month = tvbd.openingDate.substring(4, 6);
+    const day = tvbd.openingDate.substring(6, 8);
+    return dayjs(`${year}-${month}-${day}`).format("YYYY/MM/DD（dd）");
+}
+
+export default defineEventHandler(async (event: any) => {
+    let p: Promise<any> = new Promise((resolve, reject) => {
+        const query: string = `
+            SELECT
+                main.opening_date,
+                IFNULL(d.code, '00') division_code,
+                IFNULL(d.name, 'その他') division_name,
+                main.trading_value
+            FROM
+                (
+                    SELECT
+                        sdi.opening_date,
+                        s.division_code,
+                        SUM(sdi.trading_value) trading_value
+                    FROM
+                        symbol_daily_info sdi
+                        INNER JOIN
+                            symbols s
+                        ON  sdi.symbol_code = s.code
+                    WHERE
+                        EXISTS(
+                            SELECT
+                                *
+                            FROM
+                                (
+                                    SELECT
+                                        opening_date
+                                    FROM
+                                        symbol_daily_info
+                                    GROUP BY
+                                        opening_date
+                                    ORDER BY
+                                        opening_date DESC
+                                    LIMIT 20
+                                ) t
+                            WHERE
+                                t.opening_date = sdi.opening_date
+                        )
+                    GROUP BY
+                        sdi.opening_date,
+                        s.division_code
+                ) main
+                LEFT JOIN
+                    divisions d
+                ON  main.division_code = d.code
+            ORDER BY
+                main.opening_date,
+                d.code
+        `;
+        pool.query(query, [], (err, rows, fields) => {
+            resolve(rows);
+        });
+    });
+    const tvbd: TradingValueByDivision[] = await p.then((result) => {
+        return result?.map((e: any) => <TradingValueByDivision>{
+            openingDate: String(e.opening_date),
+            divisionCode: String(e.division_code),
+            divisionName: String(e.division_name),
+            tradingValue: Number(e.trading_value)
+        });
+    });
+
+    const prime = {
+        openingDate: tvbd.filter(e => e.divisionCode == "01").map(getOpeningDates),
+        tradingValue: tvbd.filter(e => e.divisionCode == "01").map(e => e.tradingValue),
+    }
+    const standard = {
+        openingDate: tvbd.filter(e => e.divisionCode == "02").map(getOpeningDates),
+        tradingValue: tvbd.filter(e => e.divisionCode == "02").map(e => e.tradingValue),
+    }
+    const growth = {
+        openingDate: tvbd.filter(e => e.divisionCode == "03").map(getOpeningDates),
+        tradingValue: tvbd.filter(e => e.divisionCode == "03").map(e => e.tradingValue),
+    }
+
+    return { prime, standard, growth }
+});
