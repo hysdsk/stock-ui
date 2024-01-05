1642889ed8ff07317cecc7e389b0f45541ab0d61
hysdsk <26004436+hysdsk@users.noreply.github.com>
2024-01-05 21:52:03 +0900

    chore: 受信画面のvwap傾き列を削除する

--- pages/notices.vue
+++ pages/notices.vue
@@ -144,11 +144,6 @@
               </template>
             </el-table-column>
           </el-table-column>
-          <el-table-column prop="vwapSlopeByMin" label="VWAP傾き" header-align="center" align="right" width="130" sortable>
-            <template #default="scope">
-              <span class="">{{ scope.row.vwapSlopeByMin }}</span>
-            </template>
-          </el-table-column>
         </el-table>
       </el-card>
     </el-col>
@@ -258,7 +253,6 @@
             marketOrderBuyQty: 0,
             avgLimitOrderSellQty: 0,
             avgLimitOrderBuyQty: 0,
-            vwapSlopeByMin: 0,
             bidsign: "",
             asksign: "",
             score: null,
@@ -333,7 +327,6 @@
       rankdata.marketOrderBuyQty = notice.market_order_buy_qty;
       rankdata.avgLimitOrderSellQty = notice.avg_limit_order_sell_qty;
       rankdata.avgLimitOrderBuyQty = notice.avg_limit_order_buy_qty;
-      rankdata.vwapSlopeByMin = notice.vwap_slope_by_min;
       rankdata.bidsign = notice.bidsign;
       rankdata.asksign = notice.asksign;
       if (rankdata.score != notice.score) {
