da33e993e84d5ea05f25a0735da1c68cc44f0a51
hysdsk <26004436+hysdsk@users.noreply.github.com>
2024-06-10 19:37:16 +0900

    refactor: DB接続用ファイルを作る

--- server/api/timelines.ts
+++ server/api/timelines.ts
@@ -1,48 +1,36 @@
-import * as mysql from "mysql2";
-const config = useRuntimeConfig()
-
-const pool = mysql.createPool({
-    host: config.dbHost,
-    user: config.dbUser,
-    password: config.dbPswd,
-    database: config.dbName
-});
+import { find } from "~/utils/kabu"
 
 export default defineEventHandler(async (event: any) => {
     const query = getQuery(event);
-    const results = await new Promise((resolve, reject) => {
-        const sql = `
-            SELECT
-                opening_date,
-                symbol_code,
-                tick_time,
-                opening_price,
-                high_price,
-                low_price,
-                close_price,
-                vwap,
-                large_buy_value,
-                middle_buy_value,
-                small_buy_value,
-                large_sell_value,
-                middle_sell_value,
-                small_sell_value,
-                bid_limit_order,
-                ask_limit_order,
-                bid_market_order,
-                ask_market_order,
-                bid_over_order,
-                ask_under_order
-            FROM
-                time_lines
-            WHERE
-                opening_date = ?
-            AND
-                tick_time >= ?;
-        `
-        pool.query(sql, [query.today, query.lastTime], (err, rows, fields) => {
-            resolve(rows);
-        });
-    });
+    const sql = `
+        SELECT
+            opening_date,
+            symbol_code,
+            tick_time,
+            opening_price,
+            high_price,
+            low_price,
+            close_price,
+            vwap,
+            large_buy_value,
+            middle_buy_value,
+            small_buy_value,
+            large_sell_value,
+            middle_sell_value,
+            small_sell_value,
+            bid_limit_order,
+            ask_limit_order,
+            bid_market_order,
+            ask_market_order,
+            bid_over_order,
+            ask_under_order
+        FROM
+            time_lines
+        WHERE
+            opening_date = ?
+        AND
+            tick_time >= ?;
+    `
+    const results = find(sql, [query.today, query.lastTime]);
     return results;
 });
